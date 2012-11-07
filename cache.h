275d8f1fb5d19cd8be7c4feea6d421cb9e1b2202
pjl <pjl@pdx.edu>
2012-11-07 00:16:49 -0800

    Updated cache data structures

--- cache.h
+++ cache.h
@@ -4,18 +4,40 @@
 // Definition of cache module
 //===============================================
 
+#include <stdint.h>
+
+//Constants
+#define ICACHE_NUMSETS 16384
+#define DCACHE_NUMSETS 16384
+
 // Cache Data Structure
 
 typedef struct {
 
-    
+    uint16_t[2] line_tags;    
+    uint8_t set_info;
 
 } InstrCacheSet;
 
 typedef struct {
 
+    uint16_t[4] lineTags;
+    uint8_t set_info;
 
 } DataCacheSet;
+
+
+typedef struct {
+
+    InstrCacheSet[ICACHE_NUMSETS] sets;
+
+} L1InstrCache;
+
+typedef struct {
+
+    DataCacheSet[DCACHE_NUMSETS] sets;
+
+} L1DataCache;
 
 // Public Methods
 void cache_read(int address);
