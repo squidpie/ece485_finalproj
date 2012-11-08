174364265a8c5d1de4510e2a9193e247a8a62273
Camille Day <camilleh@cecs.pdx.edu>
2012-11-08 08:59:19 -0800

    working on an L2 stub, WIP.

--- L2.c
+++ L2.c
@@ -1,6 +1,39 @@
 ﻿#include "L2.h"
+#include <stdio.h>
 
-int L2_fetch(double hitRate) {
+uint32_t _L2_service(uint32_t address, CacheStats * pStats, int RW) {
+	
+	static CacheStats * stats;
+	uint32_t mask = 0xF << 31;
+	uint32_t reversed = 0x0;
+	int i = 31;
+	int adjust = 0;
+	
+	if (pStats != NULL) { 
+		stats = pStats;
+		return 0;
+	}
+	
+	if (RW) stats->cache_reads++;
+	else stats->cache_writes++;
+	
 	double weight = (double)rand()/(double)RAND_MAX;
-	return ((weight - hitRate) < 0);
+	
+	if ((weight - L2_HIT_RATE) < 0) {
+		if (RW) stats->cache_read_hits++;
+		else stats->cache_write_hits++;
+	}
+	else {
+		if (RW) stats->cache_read_misses++;
+		else stats->cache_write_misses++;
+	}
+	
+	for(i = 31; i > 0;  i--) {
+		printf("I: %d\n",i);
+		printf("START: reversed: %X	  mask: %X\n",reversed, mask);
+		reversed += ((address & mask) >> (i - adjust++));
+		mask = mask >> 4;
+		printf("END: reversed: %X	mask: %X\n",reversed, mask);
+	}
+	return reversed;
 }
--- L2.h
+++ L2.h
@@ -1,6 +1,16 @@
 ﻿#ifndef __L2_H__
 #define __L2_H__
 
-int L2_fetch(double);
+#include "cachestats.h"
+#include <stdlib.h>
+
+
+#define L2_read(address)	_L2_service(address, NULL, 1)
+#define L2_write(address, data)	_L2_service(address, NULL, 0)
+#define L2_init(stats)	_L2_service(0, stats, 0)
+
+#define L2_HIT_RATE	1
+
+uint32_t _L2_service(uint32_t, CacheStats*, int);
 
 #endif // __L2_H__
