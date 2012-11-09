f0c07c653eb9b660155c60895f13d441aa662dd2
U-squidmuffin\cephaloprincess <cephaloprincess@squidmuffin.(none)>
2012-11-08 21:23:41 -0800

    Completed L2 cache interface and test unit

--- L2.c
+++ L2.c
@@ -1,48 +1,41 @@
 ﻿#include "L2.h"
 #include <stdio.h>
 
-uint32_t _L2_service(uint32_t address, CacheStats * pStats, int RW) {
+uint32_t _L2_service(uint32_t address, CacheStats * statsAddr, int RW) {
 	
-	static CacheStats * stats;
+	static CacheStats * L2_stats;
 	uint32_t mask = 0xF << 28;
 	uint32_t reversed = 0x0;
-	int i;
 	int shift = 28;
-	uint32_t temp;
+	int i;
 	
-	if (pStats != NULL) { 
-		stats = pStats;
+	if (statsAddr != NULL) { 
+		L2_stats = statsAddr;
 		return 0;
 	}
 	
-	printf("RW: %d	Address: %X	stats: %d\n",RW,address, stats->cache_writes);
+	(RW) ? L2_stats->cache_reads++ : L2_stats->cache_writes++; 
 	
-	(0) ? stats->cache_reads++ : stats->cache_writes++; 
-	
-	printf("RW: %d	Address: %X	stats: %d\n",RW,address, stats->cache_writes);
-	double weight = (double)rand()/(double)RAND_MAX;
-	
-	if (((weight - L2_HIT_RATE) < 0)) {
-		if (RW) stats->cache_read_hits++;
-		else stats->cache_write_hits++;
+	if ((((double)rand()/(double)RAND_MAX) - L2_HIT_RATE) < 0) {
+		if (RW) L2_stats->cache_read_hits++;
+		else L2_stats->cache_write_hits++;
 	}
 	else {
-		if (RW) stats->cache_read_misses++;
-		else stats->cache_write_misses++;
+		if (RW) L2_stats->cache_read_misses++;
+		else L2_stats->cache_write_misses++;
 	}
-	
 	
 	if (RW) {
 		for(i = 4; i > 0;  i--) {
-			temp = ((address & mask) >> shift);
-			reversed += temp;
+			reversed += ((address & mask) >> shift);
 			shift -= 8;
 			mask = mask >> 4;
 		}
+		
 		shift = 4;
+		
 		for (i = 4; i > 0; i--) {
-			temp = ((address & mask) << shift);
-			reversed += temp;
+			reversed += ((address & mask) << shift);
 			shift += 8;
 			mask = mask >> 4;
 		}
--- L2.h
+++ L2.h
@@ -9,8 +9,9 @@
 #define L2_write(address, data)	_L2_service(address, NULL, 0)
 #define L2_init(stats)	_L2_service(0, stats, 0)
 
-#define L2_HIT_RATE	1
+#define L2_HIT_RATE	0.8
 
-uint32_t _L2_service(uint32_t, CacheStats*, int);
+
+uint32_t _L2_service(uint32_t, CacheStats *, int);
 
 #endif // __L2_H__
