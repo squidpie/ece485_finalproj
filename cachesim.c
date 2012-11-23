35d9245abe34785205fe62e277da66a6e1dc4b6e
Camille Huffman Day <zombie.grl@gmail.com>
2012-11-23 15:14:28 -0800

    Started switch statement" git push

--- cachesim.c
+++ cachesim.c
@@ -5,9 +5,11 @@
 //===============================================
 
 #include "traceread.h"
+#include "trace.h"
 #include "L2.h"
 #include "cachestats.h"
 #include "cache.h"
+
 
 int main()
 {
@@ -15,7 +17,9 @@ int main()
     Trace t;
     t.traceType = 0;
     t.address = 0;
-	
+
+	uint8_t data;	
+
 	// Instruction Cache Stats
 	CacheStats L1Istats;
 	
@@ -24,16 +28,17 @@ int main()
 	
 	//init L2 cache stats
 	CacheStats L2stats;
-	
 	cache_i_init(&L1Istats);
 	cache_d_init(&L1Dstats);
 	L2_init(&L2stats);
-
-    open_trace("traces/test1.trace");
+   open_trace("traces/test1.trace");
 
     while(!next_trace(&t))
         printf("Action: %d Addr: %x\n", t.traceType, t.address);
-		
+	     switch (t.traceType) {
+				case 0: data = cache_read(t.address);
+						  printf("L1 Data Read: %X", data);
+			}
 
     close_trace();
 }
