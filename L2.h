7145b5d49cd4e1433e8532c2b55b03aa78663252
Camille Huffman Day <zombie.grl@gmail.com>
2012-11-23 13:52:04 -0800

    Updated L2 interface for new read function

--- L2.h
+++ L2.h
@@ -5,13 +5,13 @@
 #include <stdlib.h>
 
 
-#define L2_read(address)	_L2_service(address, NULL, 1)
-#define L2_write(address, data)	_L2_service(address, NULL, 0)
-#define L2_init(stats)	_L2_service(0, stats, 0)
+#define L2_read(address, data)	_L2_service(address, data, NULL, 1)
+#define L2_write(address, data)	_L2_service(address, NULL, NULL, 0)
+#define L2_init(stats)	_L2_service(0, NULL, stats, 0)
 
 #define L2_HIT_RATE	0.8
 
 
-uint32_t _L2_service(uint32_t, CacheStats *, int);
+uint32_t _L2_service(uint32_t, uint8_t *, CacheStats *, int);
 
 #endif // __L2_H__
