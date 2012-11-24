a2cce53384eb266bb17320d959ab5b57e56d2b1d
Phil Lamb <phil@phlamb.com>
2012-11-23 16:28:55 -0800

    updated cache printing

--- cacheprint.c
+++ cacheprint.c
@@ -22,7 +22,7 @@ static void print_datacache(void)
     DataCacheSet currentSet;
 
     printf("Index\t\tLRU");
-    printf(" | Set 1\tTag\tData");
+    printf(" | \033[1mSet 1\033[0m\tTag\tData");
     printf(" | Set 2\tTag\tData");
     printf(" | Set 3\tTag\tData");
     printf(" | Set 4\tTag\tData\n");
@@ -45,31 +45,42 @@ static void print_datacache(void)
 static void print_instrcache(void)
 {
     int i = 0;
+    InstrCacheSet currentSet;
 
-    printf("Index\tLRU\t|Set 1\tTag\tData|Set2\tTag\tData\n");
+    printf("Index\t\tLRU");
+    printf(" | \033[1mSet 1\033[0m\tTag\tData");
+    printf(" | Set 2\tTag\tData");
+
     for(i = 0; i < ICACHE_NUMSETS; i++)
     {
+        currentSet = L1I.sets[i];
         //If any line in the set is valid, print the set
-        if(L1I.sets[i].lines[0].tag & VALID || L1I.sets[i].lines[1].tag & VALID)
-            print_icacheset(i, &L1I.sets[i]);
+        if(currentSet.lines[0].tag & VALID
+            || currentSet.lines[1].tag & VALID)
+        {
+            print_icacheset(i, &currentSet);
+        }
     }
 }
 
 static void print_icacheset(uint16_t index, InstrCacheSet * set)
 {
-   printf("0x%X\t\t%d\t\t\t0x%X\t0x%X\t0x%X\t0x%X\n",
-    index, set->set_info, set->lines[0].tag, set->lines[0].data, set->lines[1].tag, set->lines[1].data);
+    printf("0x%X\t\t%d\t\t", index, set->set_info);
+    printf("0x%X\t0x%X\t",
+        set->lines[0].tag, set->lines[0].data);
+    printf("0x%X\t0x%X\t",
+        set->lines[1].tag, set->lines[1].data);
 }
 
 static void print_dcacheset(uint16_t index, DataCacheSet * set)
 {
     printf("0x%X\t\t%d\t\t", index, set->set_info);
     printf("0x%X\t0x%X\t", 
-        set->lines[0].tag, set->lines[0].tag);
+        set->lines[0].tag, set->lines[0].data);
     printf("0x%X\t0x%X\t", 
-        set->lines[1].tag, set->lines[1].tag);
+        set->lines[1].tag, set->lines[1].data);
     printf("0x%X\t0x%X\t", 
-        set->lines[2].tag, set->lines[2].tag);
+        set->lines[2].tag, set->lines[2].data);
     printf("0x%X\t0x%X\t\n", 
-        set->lines[3].tag, set->lines[3].tag);
+        set->lines[3].tag, set->lines[3].data);
 }
